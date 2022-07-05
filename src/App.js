feb17e830500487813c74f179deeb622c6b00a1b
Amixdu <amixxdu@gmail.com>
2022-07-06 05:18:47 +0530

    Routing complete

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useRef, useEffect} from 'react'
+import { BrowserRouter as Router, Route, Routes, Switch } from "react-router-dom";
+import Login from './Login';
+import Home from './Home';
+import MainPage from './MainPage';
+import Signup from './Signup';
 
 function App() {
+
+
+  let randomId = () => {
+    let s4 = () => {
+        return Math.floor((1 + Math.random()) * 0x10000)
+            .toString(16)
+            .substring(1);
+    }
+    return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+  }
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+
+        {/* Enter Nav Bar outside of Switch */}
+
+        <Switch>
+
+          <Route exact path="/">
+            <MainPage />
+          </Route>
+
+          <Route path="/login">
+            <Login />
+          </Route>
+
+          <Route path="/signup">
+            <Signup />
+          </Route>
+
+          <Route path="/home">
+            <Home />
+          </Route>
+
+        </Switch>
+
+      </div>
+    </Router>
+    
   );
 }
 
---
+++ src/Login.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Login() {
+
+  const handleSubmit = (e) => {
+    console.log('Handle')
+    e.preventDefault()
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="email" name="email" placeholder='Email'  /><br />
+        <input type="password" name="pwd" placeholder='Password'  /><br />
+        <button>Log In</button>
+      </form>
+    </div>
+  )
+}
