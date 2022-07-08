060aa59671b1e6f79abdb0f107cfbc899afe147f
Amixdu <amixxdu@gmail.com>
2022-07-08 21:29:50 +0530

    Sign up works

--- src/Signup.js
+++ src/Signup.js
@@ -3,38 +3,22 @@ import React, { useRef, useState } from 'react'
 import { Form, Button, Card } from 'react-bootstrap'
 
 // import { useAuth } from './contexts/AuthContext'
-// import { auth } from "./firebase"
-
-import { initializeApp } from "firebase/app";
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "./firebase"
+import { createUserWithEmailAndPassword } from "firebase/auth";
 
 
 export default function Signup() {
   const emailRef = useRef()
   const passwordRef = useRef()
   const passwordConfirmRef = useRef()
-  // const { signup } = useAuth()
   const [error, setError] = useState('')
   const [loading, setLoading] = useState(false)
 
+  // const { signup } = useAuth()
   const [currentU, setCurrentU] = useState()
 
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyDqKg8tQrdttBf1PIagX6gKoWVFYJLRmVs",
-    authDomain: "auth-showtracker.firebaseapp.com",
-    databaseURL: "https://console.firebase.google.com/u/2/project/auth-showtracker/database/auth-showtracker-default-rtdb/data/~2F",
-    projectId: "auth-showtracker",
-    storageBucket: "auth-showtracker.appspot.com",
-    messagingSenderId: "1087708722286",
-    appId: "1:1087708722286:web:675e143112fbce48e7e27f"
-  };
-
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const auth = getAuth();
   
-  function handleSubmit(e){
+  async function handleSubmit(e){
     e.preventDefault()
 
     if (passwordRef.current.value !== passwordConfirmRef.current.value){
@@ -43,20 +27,18 @@ export default function Signup() {
 
     setError('')
 
-    console.log(emailRef.current.value)
-    console.log(passwordRef.current.value)
+    // console.log(emailRef.current.value)
+    // console.log(passwordRef.current.value)
 
     // If checks passed, signup
-    
-    createUserWithEmailAndPassword(auth, emailRef.current.value, passwordRef.current.value)
-      .then((userCredential) => {
-        // Signed in 
-        console.log(userCredential)
-      })
-      .catch((error) => {
-        console.log('Nope')
-      });
 
+    try{
+      const user = await createUserWithEmailAndPassword(auth, emailRef.current.value, passwordRef.current.value)
+      setCurrentU(user)
+    }
+    catch(error){
+      console.log(error.message)
+    }
 
     setLoading(false)
 
