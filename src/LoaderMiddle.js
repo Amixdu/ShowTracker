099be06ab4483ff5eb0a5b68a3e64b0a76ed4b63
Amixdu <amixxdu@gmail.com>
2022-07-16 15:51:49 +0530

    loader centering

--- src/LoaderMiddle.js
+++ src/LoaderMiddle.js
@@ -3,8 +3,8 @@ import Spinner from 'react-bootstrap/Spinner';
 
 export default function LoaderMiddle( {col }) {
     return (
-        <div style={{ display:"flex", justifyContent:'center', alignItems:'center'}}>
-            <Spinner animation="grow" variant={col} style={{ width: "5rem", height: "5rem" }}/>;
-        </div>
+        <>
+            <Spinner animation="grow" variant={col} style={{ margin:'0', position:'absolute', top:'50%', left:'50%', msTransform:'translate(-50%, -50%)', width: "5rem", height: "5rem" }}/>;
+        </>
     )
 }
