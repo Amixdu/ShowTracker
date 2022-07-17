f640c43b2bfd1833016799c77edbf4cdbf016520
Amixdu <amixxdu@gmail.com>
2022-07-17 12:03:25 +0530

    loader size increased

--- src/LoaderMiddle.js
+++ src/LoaderMiddle.js
@@ -4,7 +4,7 @@ import Spinner from 'react-bootstrap/Spinner';
 export default function LoaderMiddle( {col }) {
     return (
         <>
-            <Spinner animation="grow" variant={col} style={{ position:'absolute', top: "0", left: "0", bottom:"0", right:"0", margin:'auto' }}/>;
+            <Spinner animation="grow" variant={col} style={{ position:'absolute', top: "0", left: "0", bottom:"0", right:"0", margin:'auto', width: '5rem', height:'5rem' }}/>;
         </>
     )
 }
